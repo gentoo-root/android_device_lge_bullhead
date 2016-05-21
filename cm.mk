e248b5e20bb312827ac9c8d0e18c8cca21c8ff06
Maxim Mikityanskiy <maxtram95@gmail.com>
2016-05-21 23:25:26 +0300

    Don't override build fingerprint
    As we generate vendor.img by ourselves now, the build fingerprint should
    not be overriden, otherwise fingerprints in /system/build.prop and
    /vendor/build.prop will not match, and a warning will show up at each
    boot.

--- cm.mk
+++ cm.mk
@@ -17,9 +17,3 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19T/2741993:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19T 2741993 release-keys" \
-    BUILD_ID=MTC19T
